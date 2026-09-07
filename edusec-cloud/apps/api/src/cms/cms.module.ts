28e59cf776b40593b1923f6eb284d3af3ddeb533
issaalazrihd-max <issaalazrihd@gmail.com>
2026-09-07 10:26:35 +0400

    Update cms.module.ts

--- edusec-cloud/apps/api/src/cms/cms.module.ts
+++ edusec-cloud/apps/api/src/cms/cms.module.ts
@@ -3,14 +3,16 @@ import { PagesController } from "./pages.controller";
 import { NavigationController } from "./navigation.controller";
 import { ProgramsController } from "./programs.controller";
 import { ContactController } from "./contact.controller";
+import { PartnersController } from "./partners.controller";
 import { PublicSiteController } from "./public-site.controller";
 import { PagesService } from "./pages.service";
 import { NavigationService } from "./navigation.service";
 import { ProgramsService } from "./programs.service";
 import { ContactService } from "./contact.service";
+import { PartnersService } from "./partners.service";
 
 @Module({
-  controllers: [PagesController, NavigationController, ProgramsController, ContactController, PublicSiteController],
-  providers: [PagesService, NavigationService, ProgramsService, ContactService],
+  controllers: [PagesController, NavigationController, ProgramsController, ContactController, PartnersController, PublicSiteController],
+  providers: [PagesService, NavigationService, ProgramsService, ContactService, PartnersService],
 })
 export class CmsModule {}
