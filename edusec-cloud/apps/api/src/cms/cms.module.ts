56ffd983cf2b2a01de815280c0fff0d98b7526ed
issaalazrihd-max <issaalazrihd@gmail.com>
2026-09-07 13:22:04 +0400

    Wire CoursesController and CoursesService into CmsModule

--- edusec-cloud/apps/api/src/cms/cms.module.ts
+++ edusec-cloud/apps/api/src/cms/cms.module.ts
@@ -2,17 +2,27 @@ import { Module } from "@nestjs/common";
 import { PagesController } from "./pages.controller";
 import { NavigationController } from "./navigation.controller";
 import { ProgramsController } from "./programs.controller";
+import { CoursesController } from "./courses.controller";
 import { ContactController } from "./contact.controller";
 import { PartnersController } from "./partners.controller";
 import { PublicSiteController } from "./public-site.controller";
 import { PagesService } from "./pages.service";
 import { NavigationService } from "./navigation.service";
 import { ProgramsService } from "./programs.service";
+import { CoursesService } from "./courses.service";
 import { ContactService } from "./contact.service";
 import { PartnersService } from "./partners.service";
 
 @Module({
-  controllers: [PagesController, NavigationController, ProgramsController, ContactController, PartnersController, PublicSiteController],
-  providers: [PagesService, NavigationService, ProgramsService, ContactService, PartnersService],
+  controllers: [
+    PagesController,
+    NavigationController,
+    ProgramsController,
+    CoursesController,
+    ContactController,
+    PartnersController,
+    PublicSiteController,
+  ],
+  providers: [PagesService, NavigationService, ProgramsService, CoursesService, ContactService, PartnersService],
 })
 export class CmsModule {}
