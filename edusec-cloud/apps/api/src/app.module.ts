03bd76dcb07a5521b5ba55450802acf26e9c3694
issaalazrihd-max <issaalazrihd@gmail.com>
2026-09-16 12:28:57 +0400

    Register VheTM ERP backend module

--- edusec-cloud/apps/api/src/app.module.ts
+++ edusec-cloud/apps/api/src/app.module.ts
@@ -11,13 +11,14 @@ import { ErpModule } from "./erp/erp.module";
 import { CmsModule } from "./cms/cms.module";
 import { DashboardModule } from "./dashboard/dashboard.module";
 import { AcademicOperationsModule } from "./academic-operations/academic-operations.module";
+import { VheTMModule } from "./vhetm/vhetm.module";
 import { HealthController } from "./common/health.controller";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuditModule, BranchScopeModule,
     AuthModule, BranchesModule, StudentsModule, TrainersModule, ErpModule, CmsModule,
-    DashboardModule, AcademicOperationsModule,
+    DashboardModule, AcademicOperationsModule, VheTMModule,
   ],
   controllers: [HealthController],
 })
