f34acd511b3e5bb526897f3f97b530fdccf97a4a
issaalazrihd-max <issaalazrihd@gmail.com>
2026-09-17 23:01:08 +0400

    feat: register WhatsApp sales integration in CMS module

--- edusec-cloud/apps/api/src/cms/cms.module.ts
+++ edusec-cloud/apps/api/src/cms/cms.module.ts
@@ -6,12 +6,14 @@ import { CoursesController } from "./courses.controller";
 import { ContactController } from "./contact.controller";
 import { PartnersController } from "./partners.controller";
 import { PublicSiteController } from "./public-site.controller";
+import { WhatsAppController } from "./whatsapp.controller";
 import { PagesService } from "./pages.service";
 import { NavigationService } from "./navigation.service";
 import { ProgramsService } from "./programs.service";
 import { CoursesService } from "./courses.service";
 import { ContactService } from "./contact.service";
 import { PartnersService } from "./partners.service";
+import { WhatsAppService } from "./whatsapp.service";
 
 @Module({
   controllers: [
@@ -22,7 +24,8 @@ import { PartnersService } from "./partners.service";
     ContactController,
     PartnersController,
     PublicSiteController,
+    WhatsAppController,
   ],
-  providers: [PagesService, NavigationService, ProgramsService, CoursesService, ContactService, PartnersService],
+  providers: [PagesService, NavigationService, ProgramsService, CoursesService, ContactService, PartnersService, WhatsAppService],
 })
 export class CmsModule {}
